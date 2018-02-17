e666ab6b383f8ceeafee24c30921fa13147fa5f2
snsmith92 <sherzelsmith@gmail.com>
2018-02-17 18:15:17 +0100

    Added link to ad show page from ad categories page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :ads, only: [:index, :new, :create, :update]
-  resources :categories do
-    resources :ads, only: [:show]
-  end 
+  resources :ads, only: [:index, :new, :create, :update, :show]
+  resources :categories
 end
