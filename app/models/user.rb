5e97ef45c29c4e5f084a80d205d75cb1f883939b
snsmith92 <sherzelsmith@gmail.com>
2018-03-06 20:17:20 +0100

    Initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :login
   has_many :ads
   has_many :favourites
   validates :first_name, presence: :true
@@ -18,13 +19,10 @@ class User < ApplicationRecord
     @login || self.username || self.email
   end
 
-  def self.find_for_database_authentication(warden_conditions)
+  def self.find_for_database_authentication warden_conditions
     conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
-    end
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
   end
 
   def validate_username
