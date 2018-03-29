1d02e5de06ca77822f6beb19d3444dced88274ce
snsmith92 <sherzelsmith@gmail.com>
2018-03-29 12:01:42 +0200

    Login and sign up page styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Database associations
+  has_many :ads
+  has_many :favourites
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
