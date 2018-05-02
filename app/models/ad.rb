13f4870bacf7f41ba44f9ec58ff2907fec403cd8
snsmith92 <sherzelsmith@gmail.com>
2018-05-02 16:21:23 +0200

    Indexed ad model

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,4 +1,6 @@
 class Ad < ApplicationRecord
+  include Elasticsearch::Model
+  
   validates :title, presence: true, length: {maximum: 75}
   validates :cost, presence: true
   validates :description, presence: true, length: {minimum: 50, maximum: 1000}
